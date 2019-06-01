d435f769ce129af9700891f4ee306a544cb2d371
ihyf <565267598@qq.com>
2019-06-01 23:19:02 +0800

    update

---
+++ jsonrpc.js
@@ -0,0 +1,110 @@
+const JSONRPC = {
+    VERSION: '2.0',
+    errorMsg: {},
+    methods: {}
+};
+/*
+错误信息 参考：https://www.jsonrpc.org/specification#error_object
+| code   	|   message	         | meaning |
+| -32700	|   Parse error	     | Invalid JSON was received by the server.An error occurred on the server while parsing the JSON text.  |
+| -32600	|   Invalid Request	 | The JSON sent is not a valid Request object.  |
+| -32601	|   Method not found | The method does not exist / is not available.  |
+| -32602	|   Invalid params	 | Invalid method parameter(s).  |
+| -32603	|   Internal error	 | Internal JSON-RPC error.  |
+| -32000 to -32099   |   Server error	     | Reserved for implementation-defined server-errors.  |
+*/
+JSONRPC.errorMsg[-32700] = 'Parse Error.';
+JSONRPC.errorMsg[-32600] = 'Invalid Request.';
+JSONRPC.errorMsg[-32601] = 'Method Not Found.';
+JSONRPC.errorMsg[-32602] = 'Invalid Params.';
+JSONRPC.errorMsg[-32603] = 'Internal Error.';
+
+/**
+ * 检查给定的请求是否有效
+ * JSONRPC调用
+ * - "jsonrpc" 当前版本== ('2.0')
+ * - "id" 请求标识，为数字
+ * - "method" 指的是调用的方法名称的字符串
+ * @param  {Object} rpc
+ * @return {Boolean}
+ */
+function validRequest(rpc) {
+    return rpc.jsonrpc === JSONRPC.VERSION
+        && (typeof rpc.id === 'number' || typeof rpc.id === 'string')
+        && typeof rpc.method === 'string';
+};
+
+/**
+ * 规范化响应对象
+ * @param  {Object} rpc
+ * @param  {Object} obj
+ * @return {Object}
+ */
+function normalize(rpc, obj) {
+    obj.id = (rpc && typeof rpc.id === 'number' ? rpc.id : null);
+    obj.jsonrpc = JSONRPC.VERSION;
+    //如果错误根据错误不存在错误信息的话代码获取错误信息
+    if (obj.error && !obj.error.message) obj.error.message = JSONRPC.errorMsg[obj.error.code];
+    return obj;
+};
+
+/**
+ * JSONRPC 请求处理
+ * @param  {Object} rpc
+ * @param  {Function} respond 响应回调
+ */
+JSONRPC.handleRequest = function (rpc, respond) {
+    //版本与一些参数验证
+    if (!validRequest(rpc)) return respond(normalize(rpc, { error: { code: -32600 } }));//请求协议不规范
+    //函数查找
+    let method = JSONRPC.methods[rpc.method];
+    if (typeof method !== 'function') return respond(normalize(rpc, { error: { code: -32601 } }));// 函数或方法未找到
+    //参数解析
+    let params = [];
+    // 未命名参数
+    if (Array.isArray(rpc.params)) {
+        params = rpc.params;
+    } else if (typeof rpc.params === 'object') {
+        //通过正则表达式过滤出参数名
+        let names = method.toString().match(/\((.*?)\)/)[1].match(/[\w]+/g);
+        if (names) {
+            let obj =rpc.params;
+            Object.keys(obj).forEach((key)=>{
+                params.push(obj[key]);
+            });
+            // for (let i of names) params.push(rpc.params[i]);
+        } else {
+            //返回错误 不支持参数
+            return respond(normalize(rpc, { error: { code: -32602, message: 'This service does not support named parameters.' } }));
+        }
+    }
+    // 用给定的错误和结果进行响应
+    let reply = function (err, result) {
+        if (err) {
+            if (typeof err === 'number') {
+                respond(normalize(rpc, {
+                    error: { code: err }
+                }));
+            } else {
+                respond(normalize(rpc, {//解析异常捕获
+                    error: {
+                        code: err.code || -32603,
+                        message: err.message
+                    }
+                }));
+            }
+        } else {
+            respond(normalize(rpc, { result: result }));
+        }
+    };
+    // 追加 reply 函数作为最后一个参数
+    // params.push(reply);
+    // 调用方法
+    try {
+        reply(false, method.apply(this, params));
+        // reply(false, method.apply(this));
+    } catch (err) {
+        reply(err);
+    }
+}
+module.exports = JSONRPC;
---
+++ server.js
@@ -0,0 +1,127 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const RPC = require('./jsonrpc');
+RPC.methods = {
+    demo(t) {
+        return "hello" + (t || "word");
+    },
+    asd(a) {
+        console.log(a);
+        return 999;
+    },
+    aaa(b, c) {
+        return b + c;
+    },
+    eee: (a, v) => {
+        return a * v;
+    },
+    ccc: (o) => {
+        //异常模拟
+        return o / asdsad;
+    },
+    //几种写法
+    test(a, b, c, d, e) {
+        return { a, b, c, d, e };
+    },
+    test2:function(a, b, c, d, e) {
+        return { a, b, c, d, e };
+    },
+    test3:(a, b, c, d, e)=>{
+        return { a, b, c, d, e };
+    }
+};
+/*
+fetch('/jsonrpc', {
+    headers: {
+        "Content-Type": "application/json"
+    },
+    method: 'POST',
+    body: JSON.stringify([
+		 { "jsonrpc": "2.0", "method": "test", "id": 1, "params": [1,2,3,4,5] },
+		{ "jsonrpc": "2.0", "method": "test2", "id": 2, "params": [1,2,3,4,5] },
+		{ "jsonrpc": "2.0", "method": "test3", "id": 3, "params":  [1,2,3,4,5] },
+        { "jsonrpc": "2.0", "method": "test", "id": 4, "params": {a:1,b:2,c:3,d:4,e:5} },
+		{ "jsonrpc": "2.0", "method": "test2", "id": 5, "params": {a:1,b:2,c:3,d:4,e:5} },
+		{ "jsonrpc": "2.0", "method": "test3", "id": 6, "params": {a:1,b:2,c:3,d:4,e:5} },
+	])
+}).then(v => {
+
+    return v.json();
+}).then(v => {
+    console.log(v)
+})
+*/
+const jsonrpc = RPC.handleRequest;
+
+const routes = {//<====路由
+    "/jsonrpc"(req, res) {
+        var contentType = req.headers['content-type'] || '';
+        //接收post请求
+        if (req.method === 'POST' && contentType.indexOf('application/json') >= 0) {
+            var data = '';
+            req.setEncoding('utf8');
+            req.addListener('data', function (chunk) { data += chunk; });
+            req.addListener('end', function () {
+                //响应函数
+                let respond = function (obj) {
+                    var body = JSON.stringify(obj);
+                    res.writeHead(200, {
+                        'Content-Type': 'application/json',
+                        'Content-Length': Buffer.byteLength(body)
+                    });
+                    res.end(body);
+                };
+                try {
+                    var rpc = JSON.parse(data),//获取json数据
+                        batch = Array.isArray(rpc);//<===是否批处理
+                    if (batch) {
+                        var responses = [],
+                            len = rpc.length,
+                            pending = len;
+                        for (var i = 0; i < len; ++i) {
+                            (function (rpc) {
+                                jsonrpc(rpc, function (obj) {
+                                    responses.push(obj);
+                                    if (!--pending) {
+                                        respond(responses);
+                                    }
+                                });
+                            })(rpc[i]);
+                        }
+                    } else {
+                        jsonrpc(rpc, respond);
+                    }
+                } catch (err) {
+                    console.log(err);
+                    return respond({ err: 1110 });
+                }
+            });
+        } else {
+            res.end();
+        }
+    },
+    "/"(request, response) {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write(`服务已开启`);
+        response.end();
+    },
+    "/404"(request, response) {
+        response.writeHead(404, { 'Content-Type': 'text/html' });
+        response.write("404");
+        response.end();
+    }
+}
+// 创建服务器
+http.createServer(function (request, response) {
+    // 解析请求，包括文件名
+    var pathname = url.parse(request.url).pathname;
+    // 输出请求的文件名
+    console.log("Request for " + pathname + " received.");
+    route = routes[pathname]
+    if (route) {
+        route(request, response);
+    } else {
+        routes["/404"](request, response);
+    }
+}).listen(8889);
